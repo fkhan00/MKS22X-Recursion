661def6e0836f9fec81e5e08d94e52928d6fe859
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-31 16:51:06 -0500

    finished tail recursion of fib method
    now will work on the linear fib method by adding array list

--- Recursion.java
+++ Recursion.java
@@ -10,8 +10,19 @@ public class Recursion{
     if(Math.abs(guess * guess - num) / num <= epsilon){
       return guess;}
     return sqrtH(num, epsilon, (num / guess + guess) / 2.0);}
-    
+
+  public static int fib(int n){
+    return fibH(n, 1, 1);
+  }
+  public static int fibH(int n, int first, int second){
+    if(n == 0){
+      return n;}
+    if(n <= 2){
+      return second;
+    }
+    return fibH(n - 1, second, first + second);
+  }
   public static void main(String[] args){
-    System.out.println(sqrt(-1, 0.001));
+    System.out.println(fib(0));
   }
 }
